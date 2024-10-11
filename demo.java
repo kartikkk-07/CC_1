ea4d9e186108ee02343f635ce43dedf0c2c1d9a1
Kartik Magdum <119559799+kartikkk-07@users.noreply.github.com>
2024-10-11 10:39:57 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,6 @@
+class demo {
+  public static void main(String prasad[]){
+    System.out.println("\n This C program is generated on github platform \n");
+    System.out.println("\n This C program is executed using Google App Engine \n");
+  }
+}
